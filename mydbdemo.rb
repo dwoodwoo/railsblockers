1aa7f81f737b3866c22ca2433ee59e1b9500cf6d
Dennis Woo <dwoo@sent.com>
2014-05-20 15:47:34 -0700

    added instructions for database demo

--- mydbdemo.rb
+++ mydbdemo.rb
@@ -1,6 +1,17 @@
 # gem install data_mapper
 # gem install dm-sqlite-adapter
 #
+# then in irb (or pry)
+# require './mydbdemo.rb'
+# Song.auto_migrate!
+#
+# song = Song.new, etc.
+#
+# to see database (in a new command line):
+# sqlite3 development.db
+# pragma table_info(songs)
+#
+
 require 'dm-core'
 require 'dm-migrations'
 
